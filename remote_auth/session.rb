fcdc6c3473a1f6fcf5a594111b1c7bb7e2af749e
SamueleGalli <samuele.aggeggio@gmail.com>
2024-11-13 11:38:49 +0100

    progressing

--- remote_auth/session.rb
+++ remote_auth/session.rb
@@ -1,19 +1,15 @@
-# session.rb
 require 'sinatra'
-require_relative 'config'  # Carica il file di configurazione
+require_relative 'config'
 
-# Usa le costanti definiti in config.rb
 client = OAuth2::Client.new(UID, SECRET, site: 'https://api.intra.42.fr')
 
-# Impostiamo le sessioni per essere persistenti (a lungo termine)
 set :session_secret, SecureRandom.hex(64)
 set :sessions, expire_after: 2592000 # Timeout della sessione (30 giorni)
 
-# Controlla se c'è una sessione attiva prima di procedere con l'autenticazione
 before do
   # Se una sessione è già attiva e l'ID della sessione è diverso, impedisci l'accesso
   if $active_session_id && $active_session_id != session.id
-    halt 403, "Una sessione è già attiva. Effettua il logout prima di continuare."
+    halt 403, "<html><body><h1>Hai già una sessione attiva!</h1><p>Per favore, effettua il logout prima di accedere di nuovo.</p><a href='/logout'>Logout</a></body></html>"
   end
 end
 
@@ -45,11 +41,17 @@ end
 # Controlla se la sessione è ancora attiva quando l'utente riapre il browser
 get '/' do
   if session[:user_authenticated] && $active_session_id == session.id
-    # Se la sessione è attiva, reindirizza l'utente alla pagina principale
     redirect 'http://localhost:8000/remote_auth/IA/index.html'
   else
-    # Altrimenti, mostra la pagina di login OAuth
     authorization_url = client.auth_code.authorize_url(redirect_uri: REDIRECT_URI)
     redirect authorization_url
   end
+end
+
+# Gestione del logout
+get '/logout' do
+  session.clear
+  $active_session_id = nil
+  puts "User logged out!"
+  redirect '/'
 end
